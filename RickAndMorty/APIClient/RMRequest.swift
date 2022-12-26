848d61b7e0d5eb9ada523bfcf825f11bc63a9b1e
Robert Pelz <pelz.robert@gmail.com>
2022-12-26 22:48:15 +0100

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Robert Pelz on 26.12.22.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
